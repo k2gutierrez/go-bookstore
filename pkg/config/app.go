0d9162850b1ee2e6c7a1e2075fba56dee650bc41
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-05-06 10:34:27 -0600

    change

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -10,7 +11,12 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "carlos:Hpq5#163228@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open(mysql.Config{
+		DriverName: "mysql",
+		DSN:        "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local",
+	}, &gorm.Config{})
+
+	gorm.Open("mysql", "carlos:Hpq5#163228@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
